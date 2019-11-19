fbf13d9453df7f67cc7ef45cd74c1d6f8f30721a
Carlos Querol <carlos@querol.us>
2019-11-19 01:20:58 -0700

    Added loop to keep guessing

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,25 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1,101);
     println!("(The secret number is: {})", secret_number);
 
-    println!("Enter your guess (1-100):");
+    loop {
+        println!("Enter your guess (1-100):");
 
-    let mut guess = String::new();
-    let len = io::stdin().read_line(&mut guess)
-        .expect("Read Line failed?");
+        let mut guess = String::new();
+        let len = io::stdin().read_line(&mut guess)
+            .expect("Read Line failed?");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please enter a number!");
+        let guess: u32 = guess.trim().parse()
+            .expect("Please enter a number!");
 
-    println!("Your guess was {} (len:{})", guess, len);
+        println!("Your guess was {} (len:{})", guess, len);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small!"),
-        Ordering::Greater => println!("Too Large!"),
-        Ordering::Equal => println!("You win!")
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Large!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
