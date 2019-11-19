95ad4df0d9132c09f76d9bd8aa969d084f0d0e47
Carlos Querol <carlos@querol.us>
2019-11-19 01:47:55 -0700

    Clean up output. Added ongoing hints as guesses are made.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use rand::Rng;
 fn main() {
     println!("Welcome to the Guessing Game!");
     let secret_number = rand::thread_rng().gen_range(1,101);
+    let mut lo_num = 1;
+    let mut hi_num = 100;
 
     loop {
-        println!("Enter your guess (1-100):");
+        println!("Enter your guess ({} to {}):", lo_num, hi_num);
 
         let mut guess = String::new();
-        let len = io::stdin().read_line(&mut guess)
+        io::stdin().read_line(&mut guess)
             .expect("Read Line failed?");
 
         let guess: u32 = match guess.trim().parse() {
@@ -18,14 +20,23 @@ fn main() {
             Err(_) => continue,
         };
 
-
-        println!("Your guess was {} (len:{})", guess, len);
+        print!("Your guess was {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too Small!"),
-            Ordering::Greater => println!("Too Large!"),
+            Ordering::Less => {
+                println!(", too small!");
+                if guess > lo_num {
+                    lo_num = guess;
+                }
+            },
+            Ordering::Greater => {
+                println!(", too large!");
+                if guess < hi_num {
+                    hi_num = guess;
+                }
+            },
             Ordering::Equal => {
-                println!("You win!");
+                println!(", You win!");
                 break;
             }
         }
