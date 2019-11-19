d5dce7a4eaf2b67c66551aa13980900856698ba9
Carlos Querol <carlos@querol.us>
2019-11-19 00:35:35 -0700

    Added rand crate and rng to get target number

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Welcome to the Guessing Game!");
+    let secret_number = rand::thread_rng().gen_range(1,101);
+    println!("(The secret number is: {})", secret_number);
+
     println!("Enter your guess (1-100):");
 
     let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
+    let len = io::stdin().read_line(&mut guess)
         .expect("Read Line failed?");
 
-    println!("Your guess was {}", guess);
+    println!("Your guess was {} (len:{})", guess, len);
 }
